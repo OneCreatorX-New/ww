adc8c23d49feb89d885b612c1ebaff5eb824d1c7
OneCreatorX-New <cuentapanama19@gmail.com>
2024-11-04 09:33:12 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,32 +4,29 @@ import fetch from 'node-fetch'
 const app = express()
 const port = process.env.PORT || 3000
 
-// Función para obtener contenido de GitHub
-async function getGithubContent(path) {
-  const response = await fetch(`https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/refs/heads/main/${path}`)
+const baseUrl = 'https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/refs/heads/main/'
+
+async function getGithubContent(filename) {
+  const response = await fetch(`${baseUrl}${filename}.lua`)
   if (!response.ok) {
-    throw new Error('No se pudo obtener el contenido de GitHub')
+    if (response.status === 404) {
+      throw new Error('Archivo no encontrado')
+    }
+    throw new Error('Error al obtener el contenido de GitHub')
   }
   return response.text()
 }
 
-// Ruta para manejar solicitudes de código
-app.get('/Generate%20Code%20Image', async (req, res) => {
+app.get('/:filename', async (req, res) => {
   try {
-    const content = await getGithubContent('18973713348.lua')
+    const content = await getGithubContent(req.params.filename)
     res.type('text/plain').send(content)
   } catch (error) {
-    res.status(500).send('Error al obtener el contenido')
-  }
-})
-
-// Ruta para manejar solicitudes numéricas
-app.get('/:id', async (req, res) => {
-  try {
-    const content = await getGithubContent(`${req.params.id}.lua`)
-    res.type('text/plain').send(content)
-  } catch (error) {
-    res.status(500).send('Error al obtener el contenido')
+    if (error.message === 'Archivo no encontrado') {
+      res.status(404).send('El archivo que se intenta acceder no existe')
+    } else {
+      res.status(500).send('Error al obtener el contenido')
+    }
   }
 })
 
